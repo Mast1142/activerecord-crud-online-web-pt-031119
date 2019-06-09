b2f3d6dab62c063f9ed59c98f2b1ba5bb1b0dd62
Samuel T <ssamt1142@gmail.com>
2019-06-09 17:37:59 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -9,7 +9,7 @@ def can_be_instantiated_and_then_saved
   # binding.pry
   movie = Movie.new
   movie.title = "This is a title."
-  movie
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -21,7 +21,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = movie.create(attributes)
 end
 
 def can_be_created_in_a_block(args = __)
